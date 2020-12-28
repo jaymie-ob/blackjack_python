220b9073f57b6906574b34e0806a2bc2df666a97
Jaymie O'Brien <49525946+jaymie-ob@users.noreply.github.com>
2020-12-28 12:48:56 +0000

    Add files via upload

---
+++ blackjack_betting.py
@@ -0,0 +1,264 @@
+# Blackjack
+# From 1 to 7 players compete against a dealer
+# each player starts with a pool of money, they're out when it is gone
+
+import cards, games     
+
+class BJ_Card(cards.Card):
+    """ A Blackjack Card. """
+    ACE_VALUE = 1
+
+    @property
+    def value(self):
+        if self.is_face_up:
+            v = BJ_Card.RANKS.index(self.rank) + 1
+            if v > 10:
+                v = 10
+        else:
+            v = None
+        return v
+
+class BJ_Deck(cards.Deck):
+    """ A Blackjack Deck. """
+    def populate(self):
+        for suit in BJ_Card.SUITS: 
+            for rank in BJ_Card.RANKS: 
+                self.cards.append(BJ_Card(rank, suit))
+                
+    def repopulate(self, min_cards):
+        num_cards = 0
+        for card in self.cards:
+            num_cards += 1
+        if num_cards < min_cards:
+            self.clear()
+            self.populate()
+            self.shuffle()
+            print("not enough cards, repopulating the deck")
+            
+                
+    def deal(self, hands, per_hand = 1):
+        for rounds in range(per_hand):
+            for hand in hands:
+                if self.cards:
+                    top_card = self.cards[0]
+                    self.give(top_card, hand)
+                else:
+                    print("Can't continue deal. Out of cards!")
+    
+
+class BJ_Hand(cards.Hand):
+    """ A Blackjack Hand. """
+    def __init__(self, name):
+        super(BJ_Hand, self).__init__()
+        self.name = name
+
+    def __str__(self):
+        rep = self.name + ":\t" + super(BJ_Hand, self).__str__()  
+        if self.total:
+            rep += "(" + str(self.total) + ")"        
+        return rep
+
+    @property     
+    def total(self):
+        # if a card in the hand has value of None, then total is None
+        for card in self.cards:
+            if not card.value:
+                return None
+        
+        # add up card values, treat each Ace as 1
+        t = 0
+        for card in self.cards:
+              t += card.value
+
+        # determine if hand contains an Ace
+        contains_ace = False
+        for card in self.cards:
+            if card.value == BJ_Card.ACE_VALUE:
+                contains_ace = True
+                
+        # if hand contains Ace and total is low enough, treat Ace as 11
+        if contains_ace and t <= 11:
+            # add only 10 since we've already added 1 for the Ace
+            t += 10   
+                
+        return t
+
+    def is_busted(self):
+        return self.total > 21
+
+
+class BJ_Player(BJ_Hand):
+    """ A Blackjack Player. has money and can bet """
+    def __init__(self, name, money = 100, bet = 0):
+        super(BJ_Player, self).__init__(name)
+        self.money = money
+        self.bet = bet
+
+    #def bet(self):
+    #    print(self.name, "you have €", self.money)
+    #    bet = games.ask_number("\n" + self.name + ", how much do you want to bet?", 1, self.money)
+    #    self.bet = bet
+        
+    def is_hitting(self):
+        response = games.ask_yes_no("\n" + self.name + ", do you want a hit? (Y/N): ")
+        return response == "y"
+
+    def bust(self):
+        print(self.name, "busts.")
+        self.lose()
+
+    def lose(self):
+        print(self.name, "loses.")
+        self.money -= self.bet
+        print(self.name, " has", self.money, "left")
+
+    def win(self):
+        print(self.name, "wins.")
+        self.money += self.bet
+        print(self.name, "has", self.money, " now.")
+
+    def push(self):
+        print(self.name, "pushes.")
+        print(self.name, "has ", self.money)
+
+
+        
+class BJ_Dealer(BJ_Hand):
+    """ A Blackjack Dealer. """
+    def is_hitting(self):
+        return self.total < 17
+
+    def bust(self):
+        print(self.name, "busts.")
+
+    def flip_first_card(self):
+        first_card = self.cards[0]
+        first_card.flip()
+
+
+class BJ_Game(object):
+    """ A Blackjack Game. """
+    def __init__(self, names):      
+        self.players = []
+        for name in names:
+            player = BJ_Player(name, 5)
+            self.players.append(player)
+
+        self.dealer = BJ_Dealer("Dealer")
+
+        self.deck = BJ_Deck()
+        self.deck.populate()
+        self.deck.shuffle()
+
+    @property
+    def still_playing(self):
+        sp = []
+        for player in self.players:
+            if not player.is_busted():
+                sp.append(player)
+        return sp
+
+    def __additional_cards(self, player):
+        while not player.is_busted() and player.is_hitting():
+            self.deck.deal([player])
+            print(player)
+            if player.is_busted():
+                player.bust()
+
+ 
+                    
+           
+    def play(self):
+        again = None
+        while again != "n":
+            # check that there's enough cards in the deck for a round
+            min_cards = 4 # 4 cards for the dealer
+            for player in self.players:
+                min_cards += 4 # 4 more cards for each player
+            self.deck.repopulate(min_cards)
+
+            # remove players with no money
+            sp = []
+            for player in self.players:
+                if player.money > 0:
+                    sp.append(player)
+                else:
+                    print(player.name, "has no money left and has been removed.")
+            self.players = sp
+
+            # when all players are done, end the game
+            if self.players == []:
+                again = 'n'
+                break
+            
+            # take bets
+            for player in self.players:
+                print("\n",player.name, "you have €", player.money)
+                bet = games.ask_number("What's your bet? ",1, player.money + 1)
+                player.bet = bet
+                
+            
+            # deal initial 2 cards to everyone
+            self.deck.deal(self.players + [self.dealer], per_hand = 2)
+            self.dealer.flip_first_card()    # hide dealer's first card
+            for player in self.players:
+                print(player)
+            print(self.dealer)
+
+            # deal additional cards to players
+            for player in self.players:
+                self.__additional_cards(player)
+
+            self.dealer.flip_first_card()    # reveal dealer's first 
+
+            if not self.still_playing:
+                # since all players have busted, just show the dealer's hand
+                print(self.dealer)
+            else:
+                # deal additional cards to dealer
+                print(self.dealer)
+                self.__additional_cards(self.dealer)
+
+                if self.dealer.is_busted():
+                    # everyone still playing wins
+                    for player in self.still_playing:
+                        player.win()                    
+                else:
+                    # compare each player still playing to dealer
+                    for player in self.still_playing:
+                        if player.total > self.dealer.total:
+                            player.win()
+                        elif player.total < self.dealer.total:
+                            player.lose()
+                        else:
+                            player.push()
+
+            # remove everyone's cards
+            for player in self.players:
+                player.clear()
+            self.dealer.clear()
+
+            again = games.ask_yes_no("\nDo you want to play again?: ")
+        
+
+def main():
+    print("\t\tWelcome to Blackjack!\n")
+    
+    names = []
+    number = games.ask_number("How many players? (1 - 7): ", low = 1, high = 8)
+    for i in range(number):
+        name = input("Enter player name: ")
+        names.append(name)
+    print()
+        
+    game = BJ_Game(names)
+
+    game.play()
+    
+
+
+main()
+input("\n\nPress the enter key to exit.")
+
+
+
